461a38bab1bfcaf1b77d9ecc9a713ebc132a588c
voodoohop <voodoo@voodoohop.com>
2018-09-14 20:11:19 +0200

    decreased epochs

--- train.py
+++ train.py
@@ -158,7 +158,7 @@ trainer = Trainer(
 
 # Training loop
 print('Start training...')
-while epoch_idx < 250:
+while epoch_idx < 100:
     epoch_idx += 1
     print('Start epoch %d...' % epoch_idx)
 
